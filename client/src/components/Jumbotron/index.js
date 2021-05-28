d39944a6fd83925e6ddc3f7be20b53a5b7e5ea18
Nate J <jamis.walsh@gmail.com>
2021-05-27 23:57:30 -0700

    components

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron">
+            <img src="../src/utils/assets/books-header-1.jpg" alt=""></img>
+        </div>
+    )
+}
+
+export default Jumbotron;
